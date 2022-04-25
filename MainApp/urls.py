7cc1cff810bddd91b61130adec30d27da87596be
Shijia Wang <Shijia_Wang1@baylor.edu>
2022-04-25 16:01:20 -0500

    0425

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -6,6 +6,6 @@ from. import views
 app_name ='MainApp'
 
 urlpatterns =[
-    path('',views.index, name= 'index'),
-    path('topics',views.topics, name= 'topics'),
+    path('',views.index, name='index'),
+    path('topics',views.topics, name='topics'),
 ]
