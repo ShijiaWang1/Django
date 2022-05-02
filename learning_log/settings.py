94d30eeb95c6ff58f4b85dbbad2ebafc58679af9
Shijia Wang <Shijia_Wang1@baylor.edu>
2022-05-02 15:47:24 -0500

    0502

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #my_apps
-    'MainApp',
+    'MainApp', 
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import URLPattern, path, include
+
+app_name ='users'
+
+urlpatterns =[
+    path('',include('django.contrib.auth.urls')),
+
+    
+]
